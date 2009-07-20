3d5b0b6e5c191ff4d47754f00ebb0b341ccd6479
steve.lamm <steve.lamm@30d21c30-5c56-11de-8d7d-01d876f1962d>
2009-07-20 21:37:56 +0000

    fix ranker include

--- models/result_ranker.py
+++ models/result_ranker.py
@@ -29,8 +29,7 @@ from models import result_ranker_storage
 
 # Two alternative rankers here
 import score_ranker
-import third_party
-from google_app_engine_ranklist import ranker
+from third_party.google_app_engine_ranklist import ranker
 
 
 class ResultRankerParent(db.Model):
