8a1eef21d63b8321df63f2c005c4579b52faf443
jackson@chromium.org <jackson@chromium.org@30d21c30-5c56-11de-8d7d-01d876f1962d>
2009-10-22 16:06:38 +0000

    changed config.js to point to mirrorrr.browsersrc.com.

--- categories/security/static/config.js
+++ categories/security/static/config.js
@@ -1,3 +1,3 @@
 // Change this constant to move the tests to your own server.
-var CROSS_ORIGIN_BASE_URL = "http://browserscope.spoofguard.org/security/static/";
+var CROSS_ORIGIN_BASE_URL = "http://mirrorrr.browsersrc.com/www.browserscope.org/security/static/";
 var SAME_ORIGIN_BASE_URL = "http://www.browserscope.org/security/static/";
