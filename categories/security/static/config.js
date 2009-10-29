c6f4a0a239fc5813998ba18052e7b702346dd9e0
jackson@chromium.org <jackson@chromium.org@30d21c30-5c56-11de-8d7d-01d876f1962d>
2009-10-29 00:31:28 +0000

    update xss filter test to work on a non app engine domain to avoid X-XSS-Protection: 0 header

--- categories/security/static/config.js
+++ categories/security/static/config.js
@@ -1,3 +1,4 @@
 // Change this constant to move the tests to your own server.
-var CROSS_ORIGIN_BASE_URL = "http://mirrorrr.browsersrc.com/www.browserscope.org/security/static/";
 var SAME_ORIGIN_BASE_URL = "http://www.browserscope.org/security/static/";
+var CROSS_ORIGIN_BASE_URL = "http://mirrorrr.browsersrc.com/www.browserscope.org/security/static/";
+var NON_APP_ENGINE_BASE_URL = "http://www.browserscope2.org/security/static/";
