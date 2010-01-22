fc7c12448cbc9a9f36d1e34e2103972bc1fc3b7a
steve.lamm <steve.lamm@30d21c30-5c56-11de-8d7d-01d876f1962d>
2010-01-22 05:27:52 +0000

    Add 2 files that I missed in the mega branch landing earlier.

---
+++ models/result_stats.py
@@ -0,0 +1,320 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License')
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Shared models."""
+
+import logging
+import sys
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+from google.appengine.api.labs import taskqueue
+
+from categories import all_test_sets
+from models.user_agent import UserAgent
+
+BROWSER_NAV = (
+  # version_level, label
+  ('top', 'Top Browsers'),
+  ('0', 'Browser Families'),
+  ('1', 'Major Versions'),
+  ('2', 'Minor Versions'),
+  ('3', 'All Versions')
+)
+
+TOP_BROWSERS = (
+  'Chrome 3', 'Chrome 4',
+  'Firefox 3.0', 'Firefox 3.5',
+  'IE 6', 'IE 7', 'IE 8',
+  'iPhone 2.2', 'iPhone 3.1',
+  'Opera 9.64', 'Opera 10',
+  'Safari 3.2', 'Safari 4.0'
+)
+
+
+class CategoryBrowserManager(db.Model):
+  """Track the browsers that belong in each category/version level."""
+
+  MEMCACHE_NAMESPACE = 'category_level_browsers'
+
+  browsers = db.StringListProperty(default=[], indexed=False)
+
+  @classmethod
+  def AddUserAgent(cls, category, user_agent):
+    """Adds a user agent's browser strings to version-level groups.
+
+    AddUserAgent assumes that it does not receive overlapping calls.
+    - It should only get called by the update-user-groups task queue.
+
+    Adds a browser for every version level.
+    If a level does not have a string, then use the one from the previous level.
+    For example, "Safari 4.3" would increment the following:
+        level  browser
+            0  Safari
+            1  Safari 4
+            2  Safari 4.3
+            3  Safari 4.3
+    Args:
+      category: a category string like 'network' or 'reflow'.
+      user_agent: a UserAgent instance.
+    """
+    key_names = [cls.KeyName(category, version_level)
+                 for version_level in range(4)]
+    level_browsers = memcache.get_multi(key_names,
+                                        namespace=cls.MEMCACHE_NAMESPACE)
+
+    browser_key_names = []
+    ua_browsers = user_agent.get_string_list()
+    max_ua_browsers_index = len(ua_browsers) - 1
+    for version_level, key_name in enumerate(key_names):
+      browser = ua_browsers[min(max_ua_browsers_index, version_level)]
+      if browser not in level_browsers.get(key_name, []):
+        browser_key_names.append((browser, key_name))
+    managers = cls.get_by_key_name([x[1] for x in browser_key_names])
+
+    updated_managers = []
+    memcache_mapping = {}
+    for (browser, key_name), manager in zip(browser_key_names, managers):
+      if manager is None:
+        manager = cls.get_or_insert(key_name)
+      if browser not in manager.browsers:
+        cls.InsortBrowser(manager.browsers, browser)
+        updated_managers.append(manager)
+        memcache_mapping[key_name] = manager.browsers
+    if updated_managers:
+      db.put(updated_managers)
+      memcache.set_multi(memcache_mapping, namespace=cls.MEMCACHE_NAMESPACE)
+
+  @classmethod
+  def GetBrowsers(cls, category, version_level):
+    """Get all the browsers for a version level.
+
+    Args:
+      category: a category string like 'network' or 'reflow'.
+      version_level: 'top', 0 (family), 1 (major), 2 (minor), 3 (3rd)
+    Returns:
+      ('Firefox 3.1', 'Safari 4.0', 'Safari 4.5', ...)
+    """
+    if version_level == 'top':
+      browsers = TOP_BROWSERS
+    else:
+      key_name = cls.KeyName(category, version_level)
+      browsers = memcache.get(key_name, namespace=cls.MEMCACHE_NAMESPACE)
+      if browsers is None:
+        manager = cls.get_by_key_name(key_name)
+        browsers = manager and manager.browsers or []
+        memcache.set(key_name, browsers, namespace=cls.MEMCACHE_NAMESPACE)
+    return browsers
+
+  @classmethod
+  def GetAllBrowsers(cls, category):
+    """Get all the browsers for a category.
+
+    Args:
+      category: a category string like 'network' or 'reflow'.
+    Returns:
+      ('Firefox', 'Firefox 3', 'Firefox 3.1', 'Safari', 'Safari 4', ...)
+      # Order is undefined
+    """
+    all_browsers = set()
+    for version_level in range(4):
+      all_browsers.update(cls.GetBrowsers(category, version_level))
+    return list(all_browsers)
+
+  @classmethod
+  def GetFilteredBrowsers(cls, category, filter):
+    """Get browsers based on a filter (prefixes for now).
+
+    Args:
+      category: a category string like 'network' or 'reflow'.
+      filter: a string such as 'Firefox', 'Opera 9'
+    Returns:
+      ('Firefox 3.1', 'Safari 4.0', 'Safari 4.5', ...)
+    """
+    filtered_browsers = [
+        browser for browser in cls.GetBrowsers(category, version_level=3)
+        if (browser.startswith(filter) and
+            (filter != 'Opera' or not browser.startswith('Opera Mini')))]
+    return filtered_browsers
+
+  @classmethod
+  def SetBrowsers(cls, category, version_level, browsers):
+    cls.SortBrowsers(browsers)
+    key_name = cls.KeyName(category, version_level)
+    memcache.set(key_name, browsers, namespace=cls.MEMCACHE_NAMESPACE)
+    manager = cls.get_or_insert(key_name)
+    manager.browsers = browsers
+    manager.put()
+
+
+  @classmethod
+  def SortBrowsers(cls, browsers):
+    """Sort browser strings in-place.
+
+    Args:
+      browsers: a list of strings
+          e.g. ['iPhone 3.1', 'Firefox 3.01', 'Safari 4.1']
+    """
+    browsers.sort(key=cls.BrowserKey)
+
+  @classmethod
+  def InsortBrowser(cls, browsers, browser):
+    """Insert a browser, in-place, into a sorted list of browsers.
+
+    Args:
+      browsers: a list of strings (e.g. ['iPhone 3.1', 'Safari 4.1'])
+      browser: a list of strings
+    """
+    browser_key = cls.BrowserKey(browser)
+    low, high = 0, len(browsers)
+    while low < high:
+      mid = (low + high) / 2
+      if browser_key < cls.BrowserKey(browsers[mid]):
+        high = mid
+      else:
+        low = mid + 1
+    browsers.insert(low, browser)
+
+  @classmethod
+  def BrowserKey(cls, browser):
+    VERSION_DIGITS = 8
+    MAX_VERSION = 99999999
+    family, v1, v2, v3 = UserAgent.parse_pretty(browser.lower())
+    extra = None
+    if family[-1] == ')':
+      family, extra = family.split(' ', 1)
+    return (family.lower(),
+            cls._BrowserKeyPart(v1),
+            cls._BrowserKeyPart(v2),
+            cls._BrowserKeyPart(v3),
+            extra)
+
+  @classmethod
+  def _BrowserKeyPart(cls, v):
+    if v is None:
+      return ''
+    elif v.isdigit():
+      digits = int(v or 0) + 1
+      nondigits = ' ' * 8
+    else:
+      nondigit_index = 0
+      while v[nondigit_index].isdigit():
+        nondigit_index += 1
+      digits, nondigits = int(v[:nondigit_index]), v[nondigit_index:]
+    return '%.08d %-8s' % (digits, nondigits)
+
+
+  @classmethod
+  def KeyName(cls, category, version_level):
+    return '%s_%s' % (category, version_level)
+
+  @classmethod
+  def DeleteMemcacheValue(cls, category, version_level):
+    key_name = cls.KeyName(category, version_level)
+    memcache.delete(key_name, namespace=cls.MEMCACHE_NAMESPACE)
+
+
+class CategoryStatsManager(object):
+  """Manage statistics for a category."""
+
+  MEMCACHE_NAMESPACE_PREFIX = 'category_stats'
+
+  @classmethod
+  def GetStats(cls, test_set, browsers, test_keys, use_memcache=True):
+    """Get stats table for a given test_set.
+
+    Args:
+      test_set: a TestSet instance
+      browsers: a list of browsers to use instead of version level
+      test_keys: a list of test keys to include in 'results'.
+      use_memcache: whether to use memcache or not
+    Returns:
+      {
+          browser: {
+              'summary_score': summary_score,
+              'summary_display': summary_display,
+              'total_runs': total_runs,
+              'results': {
+                  test_key_1: {
+                      'raw_score': raw_score_1,
+                      'score': score_1,
+                      'display': display_1,
+                      'expando': expando_1
+                      },
+                  test_key_2: {...},
+                  },
+              },
+          ...
+          'total_runs': total_runs_for_all_browsers_combined,
+      }
+    """
+    category = test_set.category
+    if use_memcache:
+      memcache_params = cls.MemcacheParams(category)
+      stats = memcache.get_multi(browsers, **memcache_params)
+    total_runs = 0
+    for browser in browsers:
+      if browser not in stats:
+        medians, num_scores = test_set.GetMediansAndNumScores(browser)
+        stats[browser] = test_set.GetStats(test_keys, medians, num_scores)
+      total_runs += stats[browser].get('total_runs', 0)
+    if use_memcache:
+      memcache.set_multi(stats, **memcache_params)
+    stats['total_runs'] = total_runs
+    return stats
+
+  @classmethod
+  def UpdateStatsCache(cls, category, browsers):
+    logging.info('UpdateStatsCache: category=%s, browsers=%s', category, browsers)
+    test_set = all_test_sets.GetTestSet(category)
+    test_keys = [t.key for t in test_set.VisibleTests()]
+    ua_stats = {}
+    for browser in browsers:
+      medians, num_scores = test_set.GetMediansAndNumScores(browser)
+      ua_stats[browser] = test_set.GetStats(test_keys, medians, num_scores)
+    memcache.set_multi(ua_stats, **cls.MemcacheParams(category))
+
+  @classmethod
+  def MemcacheParams(cls, category):
+    return {
+        'namespace': '_'.join((cls.MEMCACHE_NAMESPACE_PREFIX, category))
+        }
+
+  @classmethod
+  def DeleteMemcacheValues(cls, category, browsers):
+    memcache.delete_multi(browsers, **cls.MemcacheParams(category))
+
+
+def UpdateCategory(category, user_agent):
+  logging.info('result.stats.UpdateCategory')
+  CategoryBrowserManager.AddUserAgent(category, user_agent)
+  CategoryStatsManager.UpdateStatsCache(category, user_agent.get_string_list())
+
+
+def ScheduleCategoryUpdate(category, user_agent):
+  """Add a task to update a category's statistic.
+
+  The task is handled by base.cron.UserAgentGroup().
+  That method calls UpdateCategory().
+  """
+  task = taskqueue.Task(method='GET', params={
+      'category': category,
+      'user_agent_key': user_agent.key(),
+      })
+  try:
+    task.add(queue_name='user-agent-group')
+  except:
+    logging.info('Cannot add task: %s:%s' % (sys.exc_type, sys.exc_value))
---
+++ test/test_result_stats.py
@@ -0,0 +1,295 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License')
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test Result Stats."""
+
+__author__ = 'slamm@google.com (Stephen Lamm)'
+
+import logging
+import random
+import unittest
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from models import result_stats
+from models.result import ResultParent
+from third_party import mox
+import mock_data
+
+
+class CategoryBrowserManagerTest(unittest.TestCase):
+
+  def setUp(self):
+    db.delete(result_stats.CategoryBrowserManager.all(keys_only=True).fetch(1000))
+    memcache.flush_all()
+
+  def tearDown(self):
+    db.delete(result_stats.CategoryBrowserManager.all(keys_only=True).fetch(1000))
+    memcache.flush_all()
+
+  def testEmpty(self):
+    category = 'network'
+    version_level = 1
+    browsers = result_stats.CategoryBrowserManager.GetBrowsers(
+        category, version_level)
+    self.assertEqual([], browsers)
+
+  def testAddUserAgentBasic(self):
+    category = 'network'
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        category, mock_data.GetUserAgent('Firefox 3.5'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        category, mock_data.GetUserAgent('Firefox 3.0.7'))
+    for version_level, expected_browsers in enumerate((
+        ['Firefox'],
+        ['Firefox 3'],
+        ['Firefox 3.0', 'Firefox 3.5'],
+        ['Firefox 3.0.7', 'Firefox 3.5'])):
+      browsers = result_stats.CategoryBrowserManager.GetBrowsers(
+        category, version_level)
+      self.assertEqual(expected_browsers, browsers)
+
+  def testGetBrowsersTop(self):
+    expected_browsers = result_stats.TOP_BROWSERS
+    browsers = result_stats.CategoryBrowserManager.GetBrowsers(
+        category='foo', version_level='top')
+    self.assertEqual(expected_browsers, browsers)
+
+  def testGetBrowsersDbAndMemcacheUse(self):
+    category = 'network'
+    version_level = 3
+    cls = result_stats.CategoryBrowserManager
+    cls.AddUserAgent(category, mock_data.GetUserAgent('Firefox 3.5'))
+    self.assertEqual(['Firefox 3.5'], cls.GetBrowsers(category, version_level))
+
+    # Load browsers from db into memcache and return.
+    memcache.flush_all()
+    self.assertEqual(['Firefox 3.5'], cls.GetBrowsers(category, version_level))
+
+    # Load browsers memcache (db is not changed).
+    cls.get_by_key_name(cls.KeyName(category, version_level)).delete()
+    self.assertEqual(['Firefox 3.5'], cls.GetBrowsers(category, version_level))
+
+    # db and memcache are cleared.
+    memcache.flush_all()
+    self.assertEqual([], cls.GetBrowsers(category, version_level))
+
+  def testSetBrowsers(self):
+    category = 'basil'
+    version_level = 1
+    expected_browsers = ['IE 8', 'Safari 5.8.2', 'Firefox 3.0']
+    result_stats.CategoryBrowserManager.SetBrowsers(
+        category, version_level, expected_browsers)
+    browsers = result_stats.CategoryBrowserManager.GetBrowsers(
+        category, version_level)
+    self.assertEqual(sorted(expected_browsers), browsers)
+
+  def testSetBrowsersDbAndMemcacheUse(self):
+    category = 'basil'
+    version_level = 1
+    expected_browsers = ['Firefox 3.0', 'IE 8', 'Safari 5.8.2']
+    result_stats.CategoryBrowserManager.SetBrowsers(
+        category, version_level, expected_browsers)
+    browsers = result_stats.CategoryBrowserManager.GetBrowsers(
+        category, version_level)
+    self.assertEqual(expected_browsers, browsers)
+
+  def testSortBrowsers(self):
+    cls = result_stats.CategoryBrowserManager
+    expected_browsers = [
+        'Firefox',
+        'Firefox (Minefield)',
+        'Firefox (Shiretoko)',
+        'Firefox 3',
+        'Firefox 3.0a3pre',
+        'Firefox 3.0b5',
+        'Firefox 3.0pre',
+        'Firefox 3.0',
+        'Firefox 3.0.1',
+        'Firefox 3.4',
+        'Firefox 3.4.1',
+        'Firefox (Shiretoko) 3.5a1pre',
+        'Firefox (Minefield) 3.5a4',
+        'Firefox 3.5',
+        'Firefox (Namoroka) 3.5.1a3',
+        'Firefox 3.5.1',
+        'Firefox 3.5.2',
+        'iPhone 1.1',
+        'Safari 5.0',
+        ]
+    browsers = expected_browsers[:]
+    random.seed(5)
+    random.shuffle(browsers)
+    cls.SortBrowsers(browsers)
+    self.assertEqual(expected_browsers, browsers)
+
+  def testInsortBrowser(self):
+    cls = result_stats.CategoryBrowserManager
+    browsers = ['Firefox 3.0', 'Firefox 3.5', 'Safari 5.0']
+    cls.InsortBrowser(browsers, 'iPhone 1.1')
+    self.assertEqual(['Firefox 3.0', 'Firefox 3.5', 'iPhone 1.1', 'Safari 5.0'],
+                     browsers)
+
+
+class CategoryBrowserManagerFilterTest(unittest.TestCase):
+
+  def setUp(self):
+    self.cls = result_stats.CategoryBrowserManager
+    self.category = 'network'
+    db.delete(self.cls.all(keys_only=True).fetch(1000))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Firefox 2.5.1'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Firefox 3.1.8'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Firefox 3.0.7'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Firefox 3.1.7'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('IE 7.0'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Opera 9.70'))
+    result_stats.CategoryBrowserManager.AddUserAgent(
+        self.category, mock_data.GetUserAgent('Opera Mini 4.0.10031'))
+    memcache.flush_all()
+
+  def tearDown(self):
+    db.delete(self.cls.all(keys_only=True).fetch(1000))
+    memcache.flush_all()
+
+  def testGetFilteredBrowsersFamily(self):
+    expected_browsers = [
+        'Firefox 2.5.1', 'Firefox 3.0.7', 'Firefox 3.1.7', 'Firefox 3.1.8']
+    browsers = self.cls.GetFilteredBrowsers(self.category, 'Firefox')
+    self.assertEqual(expected_browsers, browsers)
+
+  def testGetFilteredBrowsersMajorVersion(self):
+    expected_browsers = ['Firefox 3.0.7', 'Firefox 3.1.7', 'Firefox 3.1.8']
+    browsers = self.cls.GetFilteredBrowsers(self.category, 'Firefox 3')
+    self.assertEqual(expected_browsers, browsers)
+
+  def testGetFilteredBrowsersMinorVersion(self):
+    expected_browsers = ['Firefox 3.1.7', 'Firefox 3.1.8']
+    browsers = self.cls.GetFilteredBrowsers(self.category, 'Firefox 3.1')
+    self.assertEqual(expected_browsers, browsers)
+
+  def testGetFilteredBrowsersOperaSkipsOperaMini(self):
+    expected_browsers = ['Opera 9.70']  # Opera Mini 4.0.10031 is excluded
+    browsers = self.cls.GetFilteredBrowsers(self.category, 'Opera')
+    self.assertEqual(expected_browsers, browsers)
+
+
+class CategoryStatsManagerTest(unittest.TestCase):
+
+  MANAGER_QUERY = result_stats.CategoryBrowserManager.all(keys_only=True)
+
+  def setUp(self):
+    db.delete(self.MANAGER_QUERY.fetch(1000))
+
+  def tearDown(self):
+    db.delete(self.MANAGER_QUERY.fetch(1000))
+
+  def testGetStats(self):
+    test_set = mock_data.MockTestSet()
+    add_result_params = (
+        # ((apple, banana, coconut), firefox_version)
+        ((0, 0, 500), 'Firefox 3.0.7'),
+        ((1, 1, 200), 'Firefox 3.0.7'),
+        ((0, 2, 300), 'Firefox 3.0.7'),
+        ((1, 3, 100), 'Firefox 3.5'),
+        ((0, 4, 400), 'Firefox 3.5')
+        )
+    for scores, browser in add_result_params:
+      parent = ResultParent.AddResult(
+          test_set, '12.2.2.25', mock_data.GetUserAgentString(browser),
+          'apple=%s,banana=%s,coconut=%s' % scores)
+    level_1_stats = {
+        'Firefox 3': {
+            'summary_score': 605,
+            'summary_display': '302',
+            'total_runs': 5,
+            'results': {
+                'coconut': {'score': 600, 'raw_score': 300, 'display': 'd:600'},
+                'apple': {'score': 1, 'raw_score': 0, 'display': 'no'},
+                'banana': {'score': 4, 'raw_score': 2, 'display': 'd:4'}
+                }
+            },
+        'total_runs': 5,
+        }
+    self.assertEqual(level_1_stats, result_stats.CategoryStatsManager.GetStats(
+        test_set, browsers=('Firefox 3',),
+        test_keys=['apple', 'banana', 'coconut']))
+
+    level_3_stats = {
+        'Firefox 3.0.7': {
+            'summary_score': 603,
+            'summary_display': '301',
+            'total_runs': 3,
+            'results': {
+                'coconut': {'score': 600, 'raw_score': 300, 'display': 'd:600'},
+                'apple': {'score': 1, 'raw_score': 0, 'display': 'no'},
+                'banana': {'score': 2, 'raw_score': 1, 'display': 'd:2'}
+                }
+            },
+        'Firefox 3.5': {
+            'summary_score': 908,
+            'summary_display': '405',
+            'total_runs': 2,
+            'results': {
+                'coconut': {'score': 800, 'raw_score': 400, 'display': 'd:800'},
+                'apple': {'score': 100, 'raw_score': 1, 'display': 'yes'},
+                'banana': {'score': 8, 'raw_score': 4, 'display': 'd:8'}
+                }
+            },
+        'total_runs': 5,
+        }
+    self.assertEqual(level_3_stats, result_stats.CategoryStatsManager.GetStats(
+        test_set, browsers=('Firefox 3.0.7', 'Firefox 3.5'),
+        test_keys=['apple', 'banana', 'coconut']))
+
+
+class UpdateStatsCacheTest(unittest.TestCase):
+
+  MANAGER_QUERY = result_stats.CategoryBrowserManager.all(keys_only=True)
+
+  def setUp(self):
+    self.mox = mox.Mox()
+    self.test_set = mock_data.MockTestSet()
+    db.delete(self.MANAGER_QUERY.fetch(1000))
+
+  def tearDown(self):
+    db.delete(self.MANAGER_QUERY.fetch(1000))
+    self.mox.UnsetStubs()
+
+  def testBasic(self):
+    category = self.test_set.category
+    cls = result_stats.CategoryStatsManager
+    browsers = ['Earth', 'Wind', 'Fire']
+    test_keys = ['apple', 'banana', 'coconut']
+    self.mox.StubOutWithMock(self.test_set, 'GetMediansAndNumScores')
+    self.mox.StubOutWithMock(self.test_set, 'GetStats')
+    self.test_set.GetMediansAndNumScores('Earth').AndReturn(('m1', 'n1'))
+    self.test_set.GetStats(test_keys, 'm1', 'n1').AndReturn('s1')
+    self.test_set.GetMediansAndNumScores('Wind').AndReturn(('m2', 'n2'))
+    self.test_set.GetStats(test_keys, 'm2', 'n2').AndReturn('s2')
+    self.test_set.GetMediansAndNumScores('Fire').AndReturn(('m3', 'n3'))
+    self.test_set.GetStats(test_keys, 'm3', 'n3').AndReturn('s3')
+    self.mox.ReplayAll()
+    cls.UpdateStatsCache(category, browsers)
+    self.mox.VerifyAll()
+    ua_stats = memcache.get_multi(browsers, **cls.MemcacheParams(category))
+    self.assertEqual({'Earth': 's1', 'Wind': 's2', 'Fire': 's3'},
+                     ua_stats)
