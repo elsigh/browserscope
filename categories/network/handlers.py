9fd5e03ac70d8cde3a690dc2740e849a4ea2c026
elsigh <elsigh@30d21c30-5c56-11de-8d7d-01d876f1962d>
2010-07-27 22:52:20 +0000

    browserscope.net for cgi in network switch

--- categories/network/handlers.py
+++ categories/network/handlers.py
@@ -36,8 +36,8 @@ CATEGORY = 'network'
 
 TEST_PAGE = '/%s/frameset' % CATEGORY
 
-RESOURCE_CGI_BASE = 'cuzillion.com/bin/resource.cgi'
-#RESOURCE_CGI_BASE = 'cgi.browserscope.net/cgi-bin/resource.cgi'
+#RESOURCE_CGI_BASE = 'cuzillion.com/bin/resource.cgi'
+RESOURCE_CGI_BASE = 'cgi.browserscope.net/cgi-bin/resource.cgi'
 #RESOURCE_CGI_BASE = 'resource-cgi.appspot.com'
 
 RESOURCE_CGI = '1.%s' % RESOURCE_CGI_BASE
