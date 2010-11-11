2c3206d063d1c516d00c0a5f04a814acc210eb2f
elsigh <elsigh@30d21c30-5c56-11de-8d7d-01d876f1962d>
2010-11-11 17:20:42 +0000

    Expose "refresh" for JSPerf and external usage

--- templates/stats_gviz_table.js
+++ templates/stats_gviz_table.js
@@ -1,5 +1,12 @@
 
+// This script is included in stats_gviz_table.html and stats_table.js
+
+
 var tableContainer = document.getElementById('bs-rt-{{ category }}');
+
+// Store a reference to the initialize function in this closure on the DOM
+// node for the results table for external use, i.e. in JSPerf.
+tableContainer.refresh = initialize;
 
 // ua
 var script = document.createElement('script');
@@ -12,11 +19,13 @@ script = document.createElement('script');
 script.src = '//www.google.com/jsapi';
 script.onload = function() {
   google.load('visualization', '1',
-    {'packages': ['table'],'callback': initialize});
+    {'packages': ['table'], 'callback': initialize});
 }
 tableContainer.parentNode.insertBefore(script, tableContainer);
 
 function initialize() {
+  tableContainer.innerHTML = 'Loading Browserscope results data ...';
+  tableContainer.className = 'rt-loading';
   var dataUrl = '//{{ server }}/gviz_table_data?' +
       'category={{ category }}&' +
       'ua={{ ua_params }}&' +
@@ -35,14 +44,15 @@ function draw(response) {
     alert('Sorry but there was an error getting the results data ' +
           'from Browserscope.');
   }
-
+  tableContainer.className = ''; // remove loading.
   var dataTable = new google.visualization.Table(tableContainer);
   var data = response.getDataTable();
   var cssClassNames = {
     headerRow: '',
     hoverTableRow: '',
     selectedTableRow: '',
-    tableRow: 'rt-row'
+    tableRow: 'rt-row',
+    frozenColumns: 1
   };
   dataTable.draw(data, {
     {% if w %}'width': '{{ w }}',{% endif %}
