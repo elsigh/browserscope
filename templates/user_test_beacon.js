678c21d76815f0e007a6aef71365fcb400c1ce45
elsigh <elsigh@30d21c30-5c56-11de-8d7d-01d876f1962d>
2011-08-08 21:00:31 +0000

    hasOwnProperty fallback/hasKey courtesy of JD Dalton

--- templates/user_test_beacon.js
+++ templates/user_test_beacon.js
@@ -1,6 +1,64 @@
 (function() {
     {{ js_ua_override|safe }}
 
+    /**
+     * Thanks JD Dalton, this hasKey routine is copied from
+     * https://github.com/bestiejs/waldo/blob/master/waldo.js#L153-195
+     * Checks if an object has the specified key as a direct property.
+     * @private
+     * @param {Object} object The object to check.
+     * @param {String} key The key to check for.
+     * @returns {Boolean} Returns `true` if key is a direct property,
+     * else `false`.
+     */
+    function hasKey() {
+      // lazy define for modern browsers
+      if (isFunction(hasOwnProperty)) {
+        hasKey = function(object, key) {
+          return hasOwnProperty.call(Object(object), key);
+        };
+      }
+      // or for Safari 2
+      else if ({}.__proto__ == Object.prototype) {
+        hasKey = function(object, key) {
+          var result;
+          object = Object(object);
+          object.__proto__ = [object.__proto__, object.__proto__ =
+              null, result = key in object][0];
+          return result;
+        };
+      }
+      // or for others (not as accurate)
+      else {
+        hasKey = function(object, key) {
+          object = Object(object);
+          var parent = (object.constructor || Object).prototype;
+          return key in object && !(key in parent &&
+              object[key] === parent[key]);
+        };
+      }
+      // or for an Opera < 10.53 bug, found by Garrett Smith, that occurs with
+      // window objects and not the global `this`
+      if (window.window == window && !hasKey(window.window, 'Object')) {
+        var __hasKey = hasKey;
+        hasKey = function(object, key) {
+          return object == window
+            ? key in object && object[key] !== {}[key]
+            : __hasKey(object, key);
+        };
+      }
+      return hasKey.apply(null, arguments);
+    }
+    var hasOwnProperty = {}.hasOwnProperty;
+    var toString = {}.toString;
+    function isFunction(value) {
+      return toString.call(value) == '[object Function]';
+    }
+
+
+    /**
+     * Browserscope-specific code.
+     */
     var test_key = '{{ test_key }}';
     var csrf_token = '{{ csrf_token }}';
     if (!{{ test_results_var }}) {
@@ -10,7 +68,7 @@
     }
     var results = [];
     for (var key in {{ test_results_var }}) {
-      if (!{{ test_results_var }}.hasOwnProperty(key)) {
+      if (!hasKey({{ test_results_var }}, key)) {
         continue;
       }
       var val = {{ test_results_var }}[key];
@@ -48,7 +106,10 @@
       'results': results.join(','){% if sandboxid %},
       'sandboxid': '{{ sandboxid }}'{% endif %}
     };
-    for (key in inputs) {
+    for (var key in inputs) {
+      if (!hasKey(inputs, key)) {
+        continue;
+      }
       var input = iframeDoc.createElement('input');
       input.type = 'hidden';
       input.name = key;
@@ -61,7 +122,7 @@
     var jsUa = uap.getJsUaOverrides();
     if (jsUa) {
       for (var key in jsUa) {
-        if (!jsUa.hasOwnProperty(key)) {
+        if (!hasKey(jsUa, key)) {
           continue;
         }
         var input = iframeDoc.createElement('input');
